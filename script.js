423f38969f6377d4f8f146722496ed3d191a7427
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-01-09 12:57:42 +0100

    Alert function

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ function refresh() {
 }
 
 function showAlert() {
+    let alertElement = document.querySelector('.alert');
 
+    if (!alertElement) {
+        alertElement = document.createElement('div');
+        alertElement.classList.add('alert');
+        document.body.appendChild(alertElement);
+    }
+
+    alertElement.textContent = 'Alert! This is a default visible toast!';
+
+    alertElement.style.display = 'block';
+
+    setTimeout(function() {
+        alertElement.style.display = 'show';
+    }, 2000);
 }
